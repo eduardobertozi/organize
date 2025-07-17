4b9c79cbd70ef88ab92b2ea07e1726580dd4d973
eduardobertozi <webdu15@gmail.com>
2025-07-17 12:35:32 -0300

    feat: add product stock in UI

--- src/app/dashboard/[slug]/routes/products/components/product-item.tsx
+++ src/app/dashboard/[slug]/routes/products/components/product-item.tsx
@@ -1,4 +1,5 @@
 import { DollarSignIcon } from 'lucide-react'
+import { Badge } from '@/components/ui/badge'
 import type { Product } from '@/types/product'
 import { currency } from '@/utils/currency'
 import { EditProduct } from './edit-product'
@@ -13,9 +14,12 @@ export const ProductItem: React.FC<SaleItemProps> = ({ product }) => {
 			<div className="flex w-full items-center justify-between">
 				<div className="flex w-full cursor-pointer flex-col gap-2 border-b bg-accent/20 px-2 py-4 text-sm transition duration-300 ease-in-out hover:bg-accent/50">
 					<span className="mt-2 truncate">{product.description}</span>
-					<div className="flex items-center gap-2">
-						<DollarSignIcon className="text-emerald-600" size={18} />
-						<span>{currency(product.coast)}</span>
+					<div className="flex items-center justify-between gap-2">
+						<div className="flex items-center gap-2">
+							<DollarSignIcon className="text-emerald-600" size={18} />
+							<span>{currency(product.coast)}</span>
+						</div>
+						<Badge variant="secondary"> {product.quantity} un</Badge>
 					</div>
 				</div>
 			</div>
